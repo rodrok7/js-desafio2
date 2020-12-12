9122b66744921b3820a10fbdd758ec808cf88849
Cinthia Sánchez <ceenti@gmail.com>
2020-12-11 18:56:37 -0600

    reading list incomplete

--- js/readinglist.js
+++ js/readinglist.js
@@ -3,6 +3,24 @@ let idPost = params.get("postid")
 console.log(idPost)
 
 
+
+const saveToReadingList = id => {
+    $.ajax({
+                url: `https://cards-6f1a0-default-rtdb.firebaseio.com/cards/${id}.json`,
+                method: "PATCH",
+                data:JSON.stringify({"savedPost": true}),
+                success: (response) => {
+                  console.log(response);
+                 
+                },
+                error: (error) => {
+                  console.log(error);
+                }
+    });
+};
+saveToReadingList(idPost)
+
+
 // const getPostById = id => {
     
 //     $.ajax({
