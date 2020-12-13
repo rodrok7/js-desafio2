80e1bb1709c57bcd79c098a372b81a5470a3fbc3
Cinthia Sánchez <ceenti@gmail.com>
2020-12-13 16:09:25 -0600

    search refactoring

--- js/search.js
+++ js/search.js
@@ -1,40 +1,38 @@
-$( window ).on( "load", function() { 
-    console.log(cardsCollection)
-    
-    $("#search-posts").keyup( event => {
-        console.log(event.key)
+$(window).on("load", function () {
 
-        let filterObjects = {}
+    const searcher = (selector) => {
+        $(selector).keyup(event => {
             // Si mi tecla "shift" está presionada toma el valor de la siguiente tecla
-        $("#pills-feed-tab").trigger("click")
-        
-        if (event.shiftKey || event.target.value.match(/[a-z]/gmi || event.keyCode === 32 || event.keyCode === 13)) {
-            console.log(event.key)
-            // Los datos a iterar son JSON, no arrays
-            for(key in cardsCollection){
-                console.log(key)
-                let {title} = cardsCollection[key]
-                let character = event.target.value.toLowerCase();
-                let find = title.toLowerCase().includes(character);
-                console.log(find)
-                find ? filterObjects[key] = cardsCollection[key] : null
+            $("#pills-feed-tab").trigger("click");
+            let result = searchPost(event)
+            //Si se cumplieron con las condiciones de ser letras, cuando se pone mayúsculas
+            //event.keyCode === 8 tecla borrar en unicode
+            // event.keyCode === 32 tecla espace
+            //event.keyCode === 13 tecla enter para que funcione si copiamos y pegamos
+            event.key.match(/^[a-z]$/gim) || event.shiftKey || event.keyCode === 8 || event.keyCode === 32 || event.keyCode === 13 ? printCards("", result) : null
+        })
+    }
+    const searchPost = event =>{
+        let filterObjects = {};
+           
+            if (event.shiftKey ||event.target.value.match(/[a-z]/gim) || event.keyCode === 32 || event.keyCode === 13) {
+                // Los datos a iterar son JSON, no arrays
+                for (key in cardsCollection) {
+                    let { title } = cardsCollection[key];
+                    let character = event.target.value.toLowerCase();
+                    let find = title.toLowerCase().includes(character);
+                    find ? (filterObjects[key] = cardsCollection[key]) : null;
+                }
             }
-
-        }
-        if (event.keyCode === 8) {
-            console.log(event.key)
-            for(key in cardsCollection){
-                console.log(event.key)
-                let {title} = cardsCollection[key]
-                let character = event.target.value.toLowerCase();
-                let find = title.toLowerCase().includes(character);
-                find ? filterObjects[key] = cardsCollection[key] : null
+            if (event.keyCode === 8) {
+                for (key in cardsCollection) {
+                    let { title } = cardsCollection[key];
+                    let character = event.target.value.toLowerCase();
+                    let find = title.toLowerCase().includes(character);
+                    find ? (filterObjects[key] = cardsCollection[key]) : null;
+                }
             }
-        }
-        //Si se cumplieron con las condiciones de ser letras, cuando se pone mayúsculas
-        //event.keyCode === 8 tecla borrar en unicode
-        // event.keyCode === 32 tecla espace
-        //event.keyCode === 13 tecla enter para que funcione si copiamos y pegamos
-        (event.key.match(/^[a-z]$/gmi) || event.shiftKey || event.keyCode === 8 || event.keyCode === 32 || event.keyCode === 13) ? printCards("", filterObjects) : console.log("esta no es la tecla")
-    })
-})
+            return filterObjects
+    }
+    searcher("#search-posts");
+});
