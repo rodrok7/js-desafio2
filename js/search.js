8652312c64af839e3e30cc1b75b92519fe605346
Cinthia Sánchez <ceenti@gmail.com>
2020-12-13 16:40:16 -0600

    quitando líneas de más

--- js/search.js
+++ js/search.js
@@ -2,7 +2,6 @@ $(window).on("load", function () {
 
     const searcher = (selector) => {
         $(selector).keyup(event => {
-            // Si mi tecla "shift" está presionada toma el valor de la siguiente tecla
             $("#pills-feed-tab").trigger("click");
             let result = searchPost(event)
             //Si se cumplieron con las condiciones de ser letras, cuando se pone mayúsculas
